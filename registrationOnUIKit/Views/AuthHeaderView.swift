7621b060647e1b5bbbb3ddfe850ea6b4896f098e
coffegorio <coffegorio@vk.com>
2024-10-03 03:51:48 +0300

    Начало создания формы регистрации/авторизации

---
+++ registrationOnUIKit/Views/AuthHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  AuthHeaderView.swift
+//  registrationOnUIKit
+//
+//  Created by Егорио on 03.10.2024.
+//
+
+import UIKit
+
+class AuthHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ registrationOnUIKit/Views/CustomTextField.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTextField.swift
+//  registrationOnUIKit
+//
+//  Created by Егорио on 03.10.2024.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
