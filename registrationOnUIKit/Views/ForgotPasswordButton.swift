c0796797204a2cac371db79e31e2667b7dcebc32
coffegorio <coffegorio@vk.com>
2024-10-04 03:36:27 +0300

    Апдейт

---
+++ registrationOnUIKit/Views/ForgotPasswordButton.swift
@@ -0,0 +1,20 @@
+//
+//  ForgotPasswordButton.swift
+//  registrationOnUIKit
+//
+//  Created by Егорио on 03.10.2024.
+//
+
+import UIKit
+
+class ForgotPasswordButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ registrationOnUIKit/Views/LoginButton.swift
@@ -0,0 +1,20 @@
+//
+//  LoginButton.swift
+//  registrationOnUIKit
+//
+//  Created by Егорио on 03.10.2024.
+//
+
+import UIKit
+
+class LoginButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
